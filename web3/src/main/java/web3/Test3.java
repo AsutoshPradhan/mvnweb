d8d3111b031035139ae52359c99e43f74335ab1f
asupradhan <asutoshp11@yahoo.com>
2018-10-25 20:20:23 +0530

    done

--- web3/src/main/java/web3/Test3.java
+++ web3/src/main/java/web3/Test3.java
@@ -7,9 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Test3
- */
+
 @WebServlet("/Test3")
 public class Test3 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
